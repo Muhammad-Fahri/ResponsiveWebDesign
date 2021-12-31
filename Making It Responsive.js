6627182528c84b23a14602bd7b1edeb43bab1f2a
Muhammad-Fahri <mhdfahri224@gmail.com>
2021-12-31 12:12:54 +0700

    Project Pertama

---
+++ Making It Responsive.js
@@ -0,0 +1,20 @@
+Create a media query to target screens up to 800px in width.
+  @media screen and (max-width: 800px) {...}
+
+
+Values defined using "rem" units are relative to the:
+  html element
+
+
+Fill in the blanks to make the "section" element a flexbox container.
+  section {
+    display:flex;
+  }
+
+
+Which tag is used to define the viewport?
+  meta
+
+
+True/False: You can define a padding using the rem unit.
+   true
